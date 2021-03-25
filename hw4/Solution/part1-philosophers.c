978021407d91d5c3d3defc40a6c57ea55a97a4af
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2021-03-24 23:41:21 -0400

    Finalize homework 4

--- hw4/Solution/part1-philosophers.c
+++ hw4/Solution/part1-philosophers.c
@@ -42,36 +42,56 @@ void think()
 
 void prepare_to_eat(int philosopher_id, int resource_id1, int resource_id2)
 {
-    int i_own_resource_A = 0, i_own_resource_B = 0;
+    int i_own_resource_1 = 0, i_own_resource_2 = 0;
     pthread_mutex_lock(&resource_try[resource_id1]);
     if (have_resource[resource_id1] == 0) {
         have_resource[resource_id1] = 1;
-        i_own_resource_A = 1; // acquired
+        i_own_resource_1 = 1; // acquired
     }
     pthread_mutex_unlock(&resource_try[resource_id1]);
 
     pthread_mutex_lock(&resource_try[resource_id2]);
     if (have_resource[resource_id2] == 0) {
         have_resource[resource_id2] = 1;
-        i_own_resource_B = 1; // acquired
+        i_own_resource_2 = 1; // acquired
     }
     pthread_mutex_unlock(&resource_try[resource_id2]);
 
-    if (i_own_resource_A && i_own_resource_B) {
-        // Deadlock would be possible here, but no other thread
-        // should believe they own both A and B.
+    if (i_own_resource_1 && i_own_resource_2) {
         pthread_mutex_lock(&resource[resource_id1]);
         pthread_mutex_lock(&resource[resource_id2]);
-        // Success: resources acquired
     } else {
+        //If we fail
+        // first we have to change both have_resources to 0.
+        pthread_mutex_lock(&resource_try[resource_id1]);
+        have_resource[resource_id1] = 0;
+        pthread_mutex_unlock(&resource_try[resource_id1]);
+        
+        pthread_mutex_lock(&resource_try[resource_id2]);
+        have_resource[resource_id2] = 0;
+        pthread_mutex_unlock(&resource_try[resource_id2]);
+
+        // Then, we sleep, then recall this function again
+        printf("%s failed. sleeping %d secs and retrying.\n",
+            thread_name[philosopher_id], eat_retry_time[philosopher_id]);
         sleep(eat_retry_time[philosopher_id]);
+        prepare_to_eat(philosopher_id, resource_id1, resource_id2);
     }
 }
 
 void finish_eating(int resource_id1, int resource_id2)
 {
+    // After Finishing eating we first unlock resource mutexes
     pthread_mutex_unlock(&resource[resource_id1]);
     pthread_mutex_unlock(&resource[resource_id2]);
+
+    // Then, we change both have_resources to 0 because this thread is no longer having them
+    pthread_mutex_lock(&resource_try[resource_id1]);
+    have_resource[resource_id1] = 0;
+    pthread_mutex_unlock(&resource_try[resource_id1]);
+    pthread_mutex_lock(&resource_try[resource_id2]);
+    have_resource[resource_id2] = 0;
+    pthread_mutex_unlock(&resource_try[resource_id2]);
 }
 
 void * philosopher_doit(void *philosopher_args)
@@ -81,32 +101,30 @@ void * philosopher_doit(void *philosopher_args)
   time_t time_start, time_end;
   time(&time_start);
   for (i = 0; i < 5; i++) {
-    think();
+    think(); //think
     printf("%s thought!\n", thread_name[phil->id]);
-    prepare_to_eat(phil->id, phil->left_fork_id, phil->right_fork_id);
+    prepare_to_eat(phil->id, phil->left_fork_id, phil->right_fork_id); //main logic is in this funcion
     
-    // This call to pthread_mutex_trylock is my code for testing that
-    // your program is working correctly.  -- Prof. Cooperman
-    if (pthread_mutex_trylock(&resource[0]) == 0) {
+    if (pthread_mutex_trylock(&resource[phil->left_fork_id]) == 0) {
       printf("*** ERROR:  %s should be locked; but we locked it again.\n",
-             resource_name[0]); // resource_name[0] is "Resource_A"
+             resource_name[phil->left_fork_id]);
       exit(1);
     }
-    if (pthread_mutex_trylock(&resource[1]) == 0) {
+    if (pthread_mutex_trylock(&resource[phil->right_fork_id]) == 0) {
       printf("*** ERROR:  %s should be locked; but we locked it again.\n",
-             resource_name[1]); // resource_name[0] is "Resource_B"
+             resource_name[phil->right_fork_id]);
       exit(1);
     }
 
     sleep(2);
     printf("%s ate (using %s and %s)!\n",
-      thread_name[0], resource_name[0], resource_name[1]);
+      thread_name[phil->id], resource_name[phil->left_fork_id], resource_name[phil->right_fork_id]);
 
     finish_eating(phil->left_fork_id, phil->right_fork_id); // release mutex locks
   }
   time(&time_end);
   printf("*** %s finished in %d seconds.\n",
-    thread_name[0], time_end - time_start);
+    thread_name[phil->id], time_end - time_start);
 }
 
 
