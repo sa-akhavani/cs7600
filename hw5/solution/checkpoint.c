6a5ab5e49cc85a4a7ceca6ab66ebb0a71fd13b11
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2021-04-09 17:34:20 -0400

    Add chcekpoint program

---
+++ hw5/solution/checkpoint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h> // for dirname()
+#include <sys/stat.h>
+#include <unistd.h>
+#include <assert.h>
+
+int main(int argc, char* argv[]) {
+  char **target_argv = argv+1;
+  struct stat stat_buf;
+  if (target_argv[0] == NULL || stat(target_argv[0], &stat_buf) == -1) {
+    fprintf(stderr, "*** Missing target executable or no such file.\n\n");
+    exit(1);
+  }
+
+  char buf[1000];
+  buf[sizeof(buf)-1] = '\0';
+  snprintf(buf, sizeof buf, "%s/libckpt.so",
+           dirname(argv[0]));
+  // Guard against buffer overrun:
+  assert(buf[sizeof(buf)-1] == '\0');
+  setenv("LD_PRELOAD", buf, 1);
+  setenv("TARGET", argv[1], 1);
+
+  int rc = execvp(target_argv[0], target_argv);
+  // We should not reach here.
+  fprintf(stderr, "Executable '%s' not found.\n", target_argv[0]);
+  return 1;
+}
