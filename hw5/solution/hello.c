13949bc64deb5338d071a0f2f44886b5c363ccd9
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2021-04-14 00:51:58 -0400

    Change hello.c program to counting-test

--- hw5/solution/hello.c
+++ hw5/solution/hello.c
@@ -1,29 +1,34 @@
-#include <signal.h>
+// Miller-Rabin test:
+//    https://en.wikipedia.org/wiki/Primality_test#Fermat_primality_test
+//  If you want to check the number, possibly try this site:
+//    https://www.numere-prime.ro/prime-factorization-of-numbers-break-down-to-prime-factors.php
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <assert.h>
-#include <ucontext.h>
 
-int main() {
-    int fd = open("behx.txt", O_WRONLY);
-    if (fd == -1) {perror("open");}
-    char buf[1000];
-    buf[sizeof(buf)-1] = '\0';
-    snprintf(buf, sizeof buf, "askfdhsakfhsajkfh");
-    int rc = write(fd, buf, 1000);
-    
-  if (rc == -1) {
-    perror("write");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s POSITIVE_INTEGER\n", argv[0]);
     exit(1);
   }
-  while (rc < 1000) {
-    rc += write(fd, buf, 1000);
+  errno = 0;
+  long num = strtol(argv[1], NULL, 10);
+  if (errno != 0 || num > LONG_MAX-20) {
+    fprintf(stderr, "%s: Overflow: Requested number is larger than %ld\n",
+            argv[0], LONG_MAX-20);
+    exit(1);
   }
-  assert(rc == 1000);
-  close(fd);
+  assert(num>0);
+  printf("Testing for counting, starting with: %ld\n", num);
+  
+  long n;
+  for (n = num; n < num + 10; n=n+1) {
+    printf("The next counting number is: %ld\n", n);
+    sleep(1);
+  }
+  return 0;
 }
